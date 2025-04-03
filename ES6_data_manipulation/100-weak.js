20682bd3304f8af1796ab0ace67202d954673c5c
Luis Feliciano <justblasey@gmail.com>
2025-04-03 14:04:01 -0400

    Adding advanced task

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -4,11 +4,11 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  
+ 
   const count = weakMap.get(endpoint);
   weakMap.set(endpoint, count + 1);
-  
+
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-} 
+}
