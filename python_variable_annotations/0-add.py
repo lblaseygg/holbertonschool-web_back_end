78409bbabb931bd66b37976ea64f73ec0b825796
Luis Feliciano <justblasey@gmail.com>
2025-04-25 17:10:45 -0400

    Adding code

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b:float) -> float:
+    """
+    Add two floating numbers
+
+    Args:
+        a (float): The first number
+        b (float): The second number
+
+    Returns:
+        float: The sum of a and b
+    """
+    return a + b
