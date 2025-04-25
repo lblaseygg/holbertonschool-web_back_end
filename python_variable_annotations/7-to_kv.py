3aed96ad405c0250722cb938e692d2476e4e9c27
Luis Feliciano <justblasey@gmail.com>
2025-04-25 17:52:31 -0400

    Adding code

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,8 +7,8 @@ the square of a number.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple [str, float]:
-  """
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
     Returns a tuple containing a string and the square of a number.
 
     Args:
