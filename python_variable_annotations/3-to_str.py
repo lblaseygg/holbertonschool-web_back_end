718e89ed5b7e7f53e454c90089aeb70eb4872fbc
Luis Feliciano <justblasey@gmail.com>
2025-04-25 17:23:28 -0400

    Adding code

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to convert a floating point number to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating point number to a string
+    """
+    return str(n)
