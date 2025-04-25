57e40f3975e0e0be47fb2a6837a615bf0654a362
Luis Feliciano <justblasey@gmail.com>
2025-04-25 17:19:51 -0400

    Adding code

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to concatenate two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings
+
+    Args:
+        str1 (str): The first string
+        str2 (str): The second string
+    """
+    return str1 + str2
