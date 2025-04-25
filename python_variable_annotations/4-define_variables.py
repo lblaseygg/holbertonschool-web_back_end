e5ba88583e075b2b7195e320ab8399ea32815aba
Luis Feliciano <justblasey@gmail.com>
2025-04-25 17:26:50 -0400

    Adding code

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to define variables.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = Trueschool: str = "Holberton"
