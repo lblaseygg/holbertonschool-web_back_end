fb644160b61d6b06c1bdbcd2f824ef8af2f61876
Luis Feliciano <justblasey@gmail.com>
2025-04-25 17:22:13 -0400

    Adding code

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to floor a floating point number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Floor a floating point number
+    """
+    return int(n)
