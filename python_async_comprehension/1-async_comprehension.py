0ac88c202eaf55b87f943b70706e14d6256cdbbd
Luis Feliciano <justblasey@gmail.com>
2025-05-01 14:18:27 -0400

    Adding code

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Module that demonstrates the use of async comprehensions to collect random numbers.
+This module provides a coroutine that collects random numbers using async comprehension.
+"""
 from typing import List
 from 0-async_generator import async_generator
 
