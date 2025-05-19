af79138d1df8b691cba9be81b93d94d93cd9ab5b
Luis Feliciano <justblasey@gmail.com>
2025-05-19 19:46:56 -0400

    Fixing code

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,10 +1,5 @@
-/**
- * Function that displays a message on the standard output
- * @param {string} message - The message to display
- * @return {void}
- */
-const displayMessage = (message) => {
-    process.stdout.write(message + '\n');
-  };
+function displayMessage(message) {
+    console.log(message);
+  }
   
   module.exports = displayMessage;
