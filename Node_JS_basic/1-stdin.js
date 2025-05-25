362be5e9db9be00fe36c3806e162d3344a0729fc
Luis Feliciano <justblasey@gmail.com>
2025-05-25 19:44:46 -0400

    Fixing lint and tests

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', function(data) {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  var name = data.toString().trim();
+  process.stdout.write('Your name is: ' + name + '\n');
   process.exit();
 });
 
